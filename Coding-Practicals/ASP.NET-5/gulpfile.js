bf192cc66b39db977f7b032571d44f6baaf69072
Sylvia Maguiña <SMag23@gmail.com>
2016-03-01 20:27:15 -0800

    Create gulpfile.js
    concat, uglifyCss(for css minification), and del functions/tasks added

---
+++ Coding-Practicals/ASP.NET-5/gulpfile.js
@@ -0,0 +1,35 @@
+/// <binding AfterBuild='default' Clean='clean' ProjectOpened='default' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglifyCss = require('gulp-uglifycss'),
+    del = require('del');
+
+gulp.task('copyJavascript', function () {
+    gulp.src([
+        'wwwroot/lib/bootstrap.min.js',
+        'wwwroot/lib/jquery.min.js'
+    ])
+    .pipe(concat('third-party.js'))
+    .pipe(gulp.dest('wwwroot/lib'))
+});
+
+gulp.task('copyCss', function () {
+    gulp.src([
+        'wwwroot/lib/bootstrap.min.css',
+        'wwwroot/lib/blog.css'
+    ])
+    .pipe(uglifyCss())
+    .pipe(concat('site.css'))
+    .pipe(gulp.dest('wwwroot/lib'))
+});
+
+gulp.task('clean', function () {
+    return del(['wwwroot/lib']);
+})
+
+gulp.task('default', ['clean', 'copyCss', 'copyJavascript']);
